31dc1ef15ae59d5a06fd6b623e88c955f1d5c978
Deniz Yüksel <denyueksel@tsn.at>
2020-06-19 20:26:34 +0200

    StudentService und StudentController mit CRUD Operationen versehen, students.html buttons ergänzt

--- src/main/java/com/university/directory/controller/StudentsController.java
+++ src/main/java/com/university/directory/controller/StudentsController.java
@@ -3,7 +3,7 @@ package com.university.directory.controller;
 import com.university.directory.services.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class StudentsController {
@@ -19,6 +19,41 @@ public class StudentsController {
 
         model.addAttribute("students",studentService.allStudents());
         return "students";
+
+    }
+
+    @GetMapping("/students/{id}")
+    public  String getStudentByID(Model model, @PathVariable Long id){
+
+        model.addAttribute("student",studentService.getStudentByID(id));
+        return "student";
+
+    }
+
+    @PostMapping("/students")
+    public String addStudent(@RequestParam(name = "name" )String name, @RequestParam(name = "email") String email, @RequestParam(name = "subjectArea")String subjectArea, @RequestParam(name="birthdate")String birthdate){
+
+        studentService.createStudent(name,subjectArea,email,birthdate);
+
+
+        return "redirect:/students";
+    }
+
+    @PostMapping("/students/{id}")
+    public String editStudent(@PathVariable Long id, @RequestParam(name = "name" )String name, @RequestParam(name = "email") String email, @RequestParam(name = "subjectArea")String subjectArea, @RequestParam(name="birthdate")String birthdate){
+
+        studentService.editStudent(id,name,subjectArea,email,birthdate);
+
+
+        return "redirect:/students";
+    }
+
+    @DeleteMapping("/students/{id}")
+    public String deleteStudent(@PathVariable Long id){
+
+        studentService.deleteStudentByID(id);
+
+        return "redirect:/students";
 
     }
 
--- src/main/java/com/university/directory/services/StudentService.java
+++ src/main/java/com/university/directory/services/StudentService.java
@@ -23,4 +23,46 @@ public class StudentService {
     }
 
 
+    public Student getStudentByID(Long id){
+
+        return studentRepo.findById(id).get();
+    }
+
+    public Student createStudent(String name, String subjectArea, String email, String birthDate ){
+
+        Student student = new Student();
+
+        student.setName(name);
+        student.setSubjectArea(subjectArea);
+        student.setBirthDate(birthDate);
+        student.setEmail(email);
+
+        return studentRepo.save(student);
+
+    }
+
+
+    public Student editStudent(Long id, String name, String subjectArea, String email, String birthDate ){
+
+        Student student = getStudentByID(id);
+
+        student.setName(name);
+        student.setSubjectArea(subjectArea);
+        student.setBirthDate(birthDate);
+        student.setEmail(email);
+
+        return studentRepo.save(student);
+
+    }
+
+    public void deleteStudentByID(Long id){
+
+        Student student = getStudentByID(id);
+
+        studentRepo.delete(student);
+
+
+    }
+
+
 }
